1e09b6fa1f5ed84a1e3739b61b376179d0e2f32d
shshemi <shshemi@gmail.com>
2026-05-17 20:59:31 +0100

    Broaden ExecuteSystemBinaries so real binaries can actually run

--- src/policy/execute_system_binaries.rs
+++ src/policy/execute_system_binaries.rs
@@ -5,12 +5,22 @@ pub struct ExecuteSystemBinaries;
 
 impl ToSbpl for ExecuteSystemBinaries {
     fn to_sbpl(&self) -> String {
-        r#"(allow process-exec (subpath "/usr/bin"))
-(allow process-exec (subpath "/bin"))
+        r#"(allow process-exec)
+(allow process-fork)
+(allow file-read* (literal "/"))
 (allow file-read* (subpath "/usr/bin"))
 (allow file-read* (subpath "/bin"))
 (allow file-read* (subpath "/usr/lib"))
-(allow file-read* (subpath "/System/Library"))"#
+(allow file-read* (subpath "/usr/share"))
+(allow file-read* (subpath "/System"))
+(allow file-read* (subpath "/private/etc"))
+(allow file-map-executable)
+(allow file-read-metadata)
+(allow mach-lookup)
+(allow sysctl-read)
+(allow process-info*)
+(allow iokit-open)
+(allow signal)"#
             .to_string()
     }
 }
@@ -21,12 +31,22 @@ mod tests {
 
     #[test]
     fn execute_system_binaries_renders() {
-        let expected = "(allow process-exec (subpath \"/usr/bin\"))\n\
-            (allow process-exec (subpath \"/bin\"))\n\
+        let expected = "(allow process-exec)\n\
+            (allow process-fork)\n\
+            (allow file-read* (literal \"/\"))\n\
             (allow file-read* (subpath \"/usr/bin\"))\n\
             (allow file-read* (subpath \"/bin\"))\n\
             (allow file-read* (subpath \"/usr/lib\"))\n\
-            (allow file-read* (subpath \"/System/Library\"))";
+            (allow file-read* (subpath \"/usr/share\"))\n\
+            (allow file-read* (subpath \"/System\"))\n\
+            (allow file-read* (subpath \"/private/etc\"))\n\
+            (allow file-map-executable)\n\
+            (allow file-read-metadata)\n\
+            (allow mach-lookup)\n\
+            (allow sysctl-read)\n\
+            (allow process-info*)\n\
+            (allow iokit-open)\n\
+            (allow signal)";
         assert_eq!(ExecuteSystemBinaries.to_sbpl(), expected);
     }
 }
