9cd353c1c81bc0d576833a573f048b1d1d13219a
shshemi <shshemi@gmail.com>
2026-06-06 21:05:38 +0100

    clean up

--- src/http.rs
+++ src/http.rs
@@ -5,13 +5,9 @@ use crate::{AppResult, dir};
 const PROFILES_URL: &str =
     "https://raw.githubusercontent.com/shshemi/painless-belt/master/profiles";
 
-fn fetch_profile_body(name: &str) -> AppResult<String> {
-    let url = format!("{PROFILES_URL}/{name}.pb");
-    Ok(ureq::get(&url).call()?.into_body().read_to_string()?)
-}
-
 pub fn pull_profile(name: &str, dst: &str) -> AppResult<PathBuf> {
-    let body = fetch_profile_body(name)?;
+    let url = format!("{PROFILES_URL}/{name}.pb");
+    let body = ureq::get(&url).call()?.into_body().read_to_string()?;
     let dest = dir::profile_path(dst)?;
     fs::write(&dest, body)?;
     Ok(dest)
